3c37d69c5a51d18662a2c94197b55d3a2b1c1a4d
TimoBrandt1 <timobrandt2002@gmail.com>
2023-03-10 11:51:32 +0100

    Created Iframe compenent
    This iframe component shows a external site within our site. This component wil later be changed when we can implement it within our dashboard.

---
+++ src/components/screenReplication/ScreenReplication.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ScreenReplication() {
+  return (
+    <iframe
+      src="https://www.example.com/"
+      title="Example Website"
+      width="100%"
+      height="600px"
+      frameBorder="0"
+    />
+  );
+}
+
+export default ScreenReplication;
