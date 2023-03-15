4ae78901a9c2486a26bd794dba807192e393f760
TimoBrandt1 <timobrandt2002@gmail.com>
2023-03-15 12:02:50 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,10 +10,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
+        <Route path="/" element={<Layout />} >
           <Route index element={<Dashboard />} />
+        </Route>
+
+        <Route path="screen" element={<Layout />} >
           <Route index element={<Screen />} />
         </Route>
+        <Route path="*" element={<h1>404: Not Found</h1>} />
       </Routes>
     </BrowserRouter>
   );
