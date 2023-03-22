6f5c6185ba18e9fb9ecf71aea19adfdf7d2adf40
Jobbet789 <77113631+Jobbet789@users.noreply.github.com>
2023-03-22 11:32:53 +0100

    Made the screens easy
    If you want to show the screen of the form, follow what I did in
    KennisdelingScreen.jsx

---
+++ src/components/screen/KennisdelingScreen.jsx
@@ -0,0 +1,9 @@
+import BasicScreen from "./BasicScreen";
+
+function KennisdelingScreen() {
+    return (
+        <BasicScreen storageKeyPrefix="FormKennisdeling" />
+    );
+}
+
+export default KennisdelingScreen;
