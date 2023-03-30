5880d93e5b0c5b92b7867b8bb10cab41c17d8ca8
Jobbet789 <77113631+Jobbet789@users.noreply.github.com>
2023-03-30 11:00:24 +0200

    Added single files from features/backendEndpoints
    Did this instead of merging.

--- src/components/screen/KennisdelingScreen.jsx
+++ src/components/screen/KennisdelingScreen.jsx
@@ -1,8 +1,22 @@
 import BasicScreen from "./BasicScreen";
+import {useState, useEffect} from 'react';
+
 
 function KennisdelingScreen() {
+    const [data, setData] = useState({});
+    const getData = async () => {
+        const response = await fetch('http://145.89.192.107/api/formKennisdeling/13');
+        const singleData = await response.json();
+        setData(singleData[0]);
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+
     return (
-        <BasicScreen storageKeyPrefix="FormKennisdeling" />
+        <BasicScreen data={data} />
     );
 }
 
