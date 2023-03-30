e55ebfc63edd6d5b9fb7fa54ed60df83bf0fc98e
WailB15 <113335920+WailB15@users.noreply.github.com>
2023-03-30 10:45:04 +0200

    debugslide

--- src/components/debug-slide/component.DebugSlide.js
+++ src/components/debug-slide/component.DebugSlide.js
@@ -7,10 +7,9 @@ import Video from '../video/component.Video'
 function DebugSlide({ScreenName,GroupName,CurrentSlide,VideoAtributes}) {
   return (
     <div class="debug-content">
-      <MainTitle titleText={"Waiting for slides..."} titleSize={'h1'}/>
       <Video {...VideoAtributes}/>
       <footer class="debug-information">
-        <TextBlock blockText={"Screen name: "+ ScreenName} titleSize={'h3'} className={"test"}/>
+        <TextBlock blockText={"Screen name: "+ ScreenName} titleSize={'h3'}/>
         <TextBlock blockText={"Group name: "+ GroupName} titleSize={'h3'}/>
         <TextBlock blockText={"Current slide: "+ CurrentSlide} titleSize={'h3'}/>
       </footer>
