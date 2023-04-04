e832adf7e2aa3ea50607a2eacba49fb4d93244ad
Jobbet789 <77113631+Jobbet789@users.noreply.github.com>
2023-04-04 12:30:48 +0200

    Made backend routes, controllers etc

---
+++ server/app/routes/showcaseRoutes.js
@@ -0,0 +1,29 @@
+// import controller
+const showcaseProjectenController = require('../controllers/showcaseprojectenController');
+// Auth
+const verifyToken = require('../utils/auth').verifyToken;
+// Import express
+const express = require('express');
+// Import router
+const router = express.Router();
+// Import body-parser
+const bodyParser = require('body-parser');
+// Import cors
+const cors = require('cors');
+
+// Use body-parser
+router.use(bodyParser.json());
+// Use cors
+router.use(cors());
+
+router.get('/', userController.getAll);
+// Get showcase projecten by id
+router.get('/:id', showcaseProjectenController.getById);
+
+// all routes below need verifyToken in the future
+// Delete showcase projecten by id
+router.delete('/:id', showcaseProjectenController.deleteById);
+// Create showcase projecten
+router.post('/', showcaseProjectenController.create);
+// Update showcase projecten by id
+router.put('/:id', showcaseProjectenController.putById);
