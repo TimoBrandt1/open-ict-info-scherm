6293d8ff7163d4680ccfb54fd4a2928656c985b3
Jobbet <jobvdveen@gmail.com>
2023-04-06 13:16:41 +0200

    Added the basics for image rendering
    Made alot of changed,
    for making post request
    for making get request
    for rendering image

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,9 @@ import Screen from './pages/screen/Screen';
 import KennisdelingScreen from './components/screen/KennisdelingScreen';
 import Screens from "./pages/screen/Screens";
 import SlideOverzicht from "./components/slide-overzicht/component.SlideOverzicht"
+import Screens from "./pages/screen/Screens"
+import FormShowcaseProjecten from './components/Forms/FormShowcaseProjecten';
+import ShowcaseProjectenScreen from './components/screen/ShowcaseProjectenScreen'
 
 function App() {
   return (
@@ -22,12 +25,18 @@ function App() {
         <Route path="formscreen" element={<Layout />} >
           <Route index element={<KennisdelingScreen />} />
         </Route>
+        <Route path="formshowcaseprojecten" element={<Layout />} >
+          <Route index element={<FormShowcaseProjecten />} />
+        </Route>
         <Route path="*" element={<h1>404: Not Found</h1>} />
         <Route path="screens" element={<Layout />} >
           <Route index element={<Screens />} />
         </Route>
         <Route path="SlideOverzicht" element={<Layout />} >
           <Route index element={<SlideOverzicht />} />
+        </Route>
+        <Route path="showcaseprojectenscreen" element={<Layout />} >
+          <Route index element={<ShowcaseProjectenScreen />} />
         </Route>
       </Routes>
     </BrowserRouter>
--- client/src/components/forms/FormBasic.jsx
+++ client/src/components/forms/FormBasic.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Form, Button, Popup } from 'semantic-ui-react'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import { Buffer } from 'buffer'
 
 
 // go to ./FormTest.js to see how to use this component
@@ -12,33 +13,40 @@ function FormBasic({initialValues, endpoint}) {
     let hasImage = false;
     // check if initial values have an image property
     // if so, add a state for the image
-    if (initialValues.hasOwnProperty('image')) {
-        let hasImage = true;
-        const [file, setFile] = useState(null);
-    }
 
     const handleChange = (name, value) => {
         setFormValues({ ...formValues, [name]: value });
     };
 
-    const handleFileChange = (e) => {
-        setFile(e.target.files[0]);
-    }
+    const handleFileChange = (image) => {
+        const reader = new FileReader();
+        reader.readAsArrayBuffer(image);
 
-    console.log(formValues);
+        reader.onload = () => {
+            setFormValues({ ...formValues, image: { buffer: reader.result }});
+        };
+    };
 
     const saveData = () => { // save the data to local storage with the prefix
-            // make post request to server
-            if (hasImage) {
-                setFormValues({ ...formValues, image: file });
-            }
+            // make post request to server// Encode image data as Base64
+            console.log(formValues);
+            const imageData = formValues.image ? { buffer: formValues.image.buffer } : null;
+            const encodedImageData = imageData ? Buffer.from(imageData.buffer).toString('base64') : null;
+            console.log((encodedImageData.length/4)*3)
+            const requestBody = {
+                onderwerp: formValues.onderwerp,
+                details: formValues.details,
+                image: { buffer: encodedImageData }
+            };
 
+
+            console.log(requestBody);
             fetch('http://145.89.192.107/api' + endpoint, {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json'
                     },
-                    body: JSON.stringify(formValues)
+                    body: JSON.stringify(requestBody)
                     })
                     .then(response => response.json())
                     .then(data => {
@@ -49,7 +57,6 @@ function FormBasic({initialValues, endpoint}) {
                         console.error('Error:', error);
                     }
                 );
-
     }
 
     const handleSubmit = (e) => {
@@ -73,16 +80,18 @@ function FormBasic({initialValues, endpoint}) {
                     <Form onSubmit={handleSubmit} onReset={() => setFormValues(initialValues)}>
                         {inputNames.map((name) => (
                             <Form.Field>
+                                {name !== 'image' && (
                                 <Form.Input
                                     name={name}
                                     placeholder={name}
                                     onChange={(e) => handleChange(name, e.target.value)}
                                 />
-                            {hasImage && name === 'image' && (
+                            )}
+                            {name === 'image' && (
                                 <Form.Input
                                     name={name}
                                     type='file'
-                                    onChange={handleFileChange}
+                                    onChange={(e) => handleFileChange(e.target.files[0])}
                                 />
                             )}
                             </Form.Field>
