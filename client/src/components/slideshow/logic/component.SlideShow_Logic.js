97715cf47bb84a8261e9388bca14f7f4a095ea34
TimoBrandt1 <timobrandt2002@gmail.com>
2023-04-12 11:18:36 +0200

    Added new functionality.
    Loads the slides before showing the next slide

--- client/src/components/slideshow/logic/component.SlideShow_Logic.js
+++ client/src/components/slideshow/logic/component.SlideShow_Logic.js
@@ -1,20 +1,34 @@
-import React from "react";
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import './style.SlideShow_Logic.scss';
 import DebugSlide from '../../debug-slide/component.DebugSlide';
 import KennisDelingSlide from "../../kennisdeling-slide/component.KennisdelingSlide";
 
 function Slideshow({SecondsBetweenSlides}) {
   // Slides information, should later be moved to other file.
-  const [data, setData] = useState({});
-  const getData = async (id) => {
-    const response = await fetch('http://145.89.192.107/api/kennisdeling/${id}');
-    const singleData = await response.json();
-    setData(singleData[0]);
-  }
-
-  getData(13);
-  getData(17);
+  const [data1, setData1] = useState({});
+  const [data2, setData2] = useState({});
+  
+  const getData = async (id, setData) => {
+    try {
+      const response = await fetch(`http://145.89.192.107/api/kennisdeling/${id}`);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      const singleData = await response.json();
+      setData(singleData[0]);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      // Handle error as appropriate for your app
+    }
+  };
+  
+  useEffect(() => {
+    Promise.all([
+      getData(13, setData1),
+      getData(17, setData2)
+    ]).catch(error => console.error('Error fetching data:', error));
+  }, []);
+  
 
   const videoProps = {
     vSrc: { src: "videos/DebugSlideVideo.mp4" },
@@ -25,52 +39,52 @@ function Slideshow({SecondsBetweenSlides}) {
     vControls: false,
     vMuted: true
   };
+  
   const debugSlideInfo = {
     ScreenName: 'Screen 1',
     GroupName: 'Group Debugslide',
     CurrentSlide: 'Debugslide',
     VideoAtributes: videoProps
   };
+  
   const kennisDelingInfo = {
-    Subject: data["onderwerp"],
-    Speaker: data["spreker"],
-    Location: data["locatie"],
-    Time: data["tijd"],
-    Datum: data["datum"],
-    Details: data["details"]
-  };
-  const kennisDelingInfoDupe = {
-    Subject: data["onderwerp"],
-    Speaker: data["spreker"],
-    Location: data["locatie"],
-    Time: data["tijd"],
-    Datum: data["datum"],
-    Details: data["details"]
+    Subject: data1.onderwerp || "",
+    Speaker: data1.spreker || "",
+    Location: data1.locatie || "",
+    Time: data1.tijd || "",
+    Datum: data1.datum || "",
+    Details: data1.details || ""
   };
 
-  // Slide Show Logic all code under this comment
+  const kennisDelingInfoDupe = {
+    Subject: data2.onderwerp || "",
+    Speaker: data2.spreker || "",
+    Location: data2.locatie || "",
+    Time: data2.tijd || "",
+    Datum: data2.datum || "",
+    Details: data2.details || ""
+  };
+  
+  const slides = [
+    <DebugSlide {...debugSlideInfo} />,
+    <KennisDelingSlide {...kennisDelingInfo} />,
+    <KennisDelingSlide {...kennisDelingInfoDupe} />
+  ];
+  
   const [currentIndex, setCurrentIndex] = useState(0);
   
-  // Needs to be able to get Slide array in the future instead of having hard coded slides
-  const components = [
-    <DebugSlide {...debugSlideInfo}/>, 
-    <KennisDelingSlide {...kennisDelingInfo}/>,
-    <KennisDelingSlide {...kennisDelingInfoDupe}/>
-  ];
-
   useEffect(() => {
-    
-    console.log(data["onderwerp"])
-
     const timeoutId = setTimeout(() => {
-      setCurrentIndex((currentIndex + 1) % components.length);
+      getData(13, setData1);
+      getData(17, setData2);
+      setCurrentIndex((currentIndex + 1) % slides.length);
     }, SecondsBetweenSlides*1000);
 
     const handleKeyDown = (event) => {
       if (event.keyCode === 37) { // Left arrow key
-        setCurrentIndex((currentIndex - 1 + components.length) % components.length);
+        setCurrentIndex((currentIndex - 1 + slides.length) % slides.length);
       } else if (event.keyCode === 39) { // Right arrow key
-        setCurrentIndex((currentIndex + 1) % components.length);
+        setCurrentIndex((currentIndex + 1) % slides.length);
       }
     };
 
@@ -80,11 +94,11 @@ function Slideshow({SecondsBetweenSlides}) {
       clearTimeout(timeoutId);
       document.removeEventListener('keydown', handleKeyDown);
     };
-  }, [currentIndex, components]);
-
+  }, [currentIndex, SecondsBetweenSlides, slides]);
+  
   return (
     <>
-      {components[currentIndex]}
+      {slides[currentIndex]}
     </>
   );
 }
