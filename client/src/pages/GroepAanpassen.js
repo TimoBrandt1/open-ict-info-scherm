b3c4eead90809f728cf960f1e720afe6c4ee143b
JesseOpenICT <125012405+JesseOpenICT@users.noreply.github.com>
2023-05-23 10:34:45 +0200

    navbar routes fix test

---
+++ client/src/pages/GroepAanpassen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import GroepAanpassen from '../components/groep-edit/component.GroepEdit';
+
+const SlideBeheer = () => {
+  return (
+    <div>
+      <GroepAanpassen src="/groep-edit"/>
+    </div>
+  )
+};
+
+export default SlideBeheer;
