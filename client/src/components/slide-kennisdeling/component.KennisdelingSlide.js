c617a0c23560ed8cdaba2ba59228a3166c2ab7e7
JesseOpenICT <125012405+JesseOpenICT@users.noreply.github.com>
2023-06-14 12:33:29 +0200

    Slide pagina's verbeterde opmaak
    Titel

--- client/src/components/slide-kennisdeling/component.KennisdelingSlide.js
+++ client/src/components/slide-kennisdeling/component.KennisdelingSlide.js
@@ -6,17 +6,17 @@ import TextBlock from '../text-block/component.TextBlock'
 function KennisDelingSlide({slideData}) {
   return (
     <div className="kennisdeling-wrapper">
-      <MainTitle titleText={"Kennisdeling Page"} titleSize={'h1'} className={"main-title"}/>
+      <MainTitle titleText={"Kennisdelingen"} titleSize={'h1'} className={"main-title"}/>
+      <div class= "kennisdeling-miniwrapper">
       {slideData.map((slide, index) => (
-        <div className={`kennisdeling-${index+1}`} key={index}>
-          <TextBlock blockText={"Subject: "+ slide.Subject} titleSize={'h3'}/>
-          <TextBlock blockText={"Speaker: "+ slide.Speaker} titleSize={'h3'}/>
-          <TextBlock blockText={"Location: "+ slide.Location} titleSize={'h3'}/>
-          <TextBlock blockText={"Time: "+ slide.Time} titleSize={'h3'}/>
-          <TextBlock blockText={"Datum: "+ slide.Datum} titleSize={'h3'}/>
-          <TextBlock blockText={"Details: "+ slide.Details} titleSize={'h3'}/>
+        <div className={`kennisdeling`} key={index}>
+          <TextBlock blockText={"Kennisdeling "+ slide.Subject} titleSize={'h3'} className={"primary"}/>
+          <TextBlock blockText={"Spreker: "+ slide.Speaker} titleSize={'h3'}/>
+          <TextBlock blockText={slide.Location + ", " + slide. Datum + ", " + slide.Time} titleSize={'h3'}/><br/>
+          <TextBlock blockText={slide.Details} titleSize={'h3'}/>
         </div>
       ))}
+      </div>
     </div>
   );
 }
